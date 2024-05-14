0d7098ffeade04056ac0737a4c50dafc2193c85c
kfinnAmpel <kevin.finn@ampelbiosolutions.com>
2024-05-14 16:18:15 -0400

    Test deploy action

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ document.querySelector("#toggle-theme").addEventListener("click", () => {
     document.documentElement.classList.toggle("light-theme");
 });
 
+
 const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 
 if (prefersLight) {
